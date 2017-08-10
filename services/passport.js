2544593608ebff50923e4e2ea3bcaa32c656aad0
Joe Dahle <joe.dahle@gmail.com>
2017-08-09 21:53:38 -0700

    feat: Add mongo and user schema

---
+++ services/passport.js
@@ -0,0 +1,26 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we have a returning user!
+        } else {
+          // we need to create a new user
+          new User({ googleId: profile.id }).save();
+        }
+      });
+    }
+  )
+);
